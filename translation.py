ace2979060b344fe87e4adb78feb1c6744a2f73d
Panayiotiscypus1 <panayiotis@cyprus.com>
2025-01-22 16:37:28 +0100

    ok

--- translation.py
+++ translation.py
@@ -1,5 +1,20 @@
 from unidecode import unidecode
 
-greek_text = "Καλημέρα κόσμε"
-latin_text = unidecode(greek_text)
-print(latin_text)
+
+custom_mapping = {
+    'χ': 'ch',  # Replace "kh" with "ch"
+    'Θ': 'Th',  # Add custom mappings as needed
+    'φ': 'Ph',
+    # Add more mappings as needed
+}
+
+def translate(text):
+    # Perform the initial transliteration using unidecode
+    transliterated_text = unidecode(text.lower())
+    
+    # Apply custom mappings
+    for greek_char, custom_value in custom_mapping.items():
+        transliterated_text = transliterated_text.replace(unidecode(greek_char), custom_value)
+    
+    return transliterated_text
+
