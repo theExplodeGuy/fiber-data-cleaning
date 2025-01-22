7cde0c5a4295ebe17be268a68e203604c90af36a
Panayiotiscypus1 <panayiotis@cyprus.com>
2025-01-22 16:42:06 +0100

    ignore

--- address.py
+++ address.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import translation
-'''
+
 df = pd.read_excel('data.xlsx')
 unique_values = df['tk'].unique()
 count = df['tk'].value_counts().get(unique_values[0], 0)
 print(unique_values[0])
 print(count)
-'''
-print(translation.translate("Χαριλαου γκευ"))
+
+print(translation.translate("Charilaou Miχael"))
