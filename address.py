6cc6ac2ff732aeeb522334f8b11cf81f9373e543
Marios Stavrou <marios.stavrou04@gmail.com>
2025-01-22 16:04:25 +0100

    counts unique occurances of tk

--- address.py
+++ address.py
@@ -2,5 +2,6 @@ import pandas as pd
 
 df = pd.read_excel('data.xlsx')
 unique_values = df['tk'].unique()
-
-print(unique_values)
+count = df['tk'].value_counts().get(unique_values[1], 0)
+print(unique_values[0])
+print(count)
