e9d0b814ab9cb24ebaf66e2b9271beceeea2e662
Marios Stavrou <marios.stavrou04@gmail.com>
2025-01-22 16:06:14 +0100

    ime vlakas

--- address.py
+++ address.py
@@ -2,6 +2,6 @@ import pandas as pd
 
 df = pd.read_excel('data.xlsx')
 unique_values = df['tk'].unique()
-count = df['tk'].value_counts().get(unique_values[1], 0)
+count = df['tk'].value_counts().get(unique_values[0], 0)
 print(unique_values[0])
 print(count)
