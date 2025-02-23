5dcc73d182d628559000efac86982bcacc3a51cf
Marios Stavrou <marios.stavrou04@gmail.com>
2025-02-23 15:37:43 +0100

    merge

--- translation.py
+++ translation.py
@@ -1,4 +1,5 @@
 from unidecode import unidecode
+import re
 
 custom_mapping = {
     'χ': 'ch',  # Replace "kh" with "ch"
@@ -8,19 +9,21 @@ custom_mapping = {
 }
 
 def clean(text):
-    punctuation=["/", ".", ",", "*", "(", ")", "&", "#", "@", "!"]
+    punctuation=[ "Αρ.","/", ".", ",", "*", "(", ")", "&", "#", "@", "!"]
     text=text.strip()
     for punc in punctuation:
         text=text.replace(punc, "")
+    text = re.sub(r'\s+', ' ', text).strip()
     return text
 
-def translate(text):
+def translate_clean(text):
+    transliterated_text = clean(text)
     # Perform the initial transliteration using unidecode
-    transliterated_text = unidecode(text.lower())
+    transliterated_text = unidecode(transliterated_text.lower())
     
     # Apply custom mappings
     for greek_char, custom_value in custom_mapping.items():
         transliterated_text = transliterated_text.replace(unidecode(greek_char), custom_value)
-    
+
     return transliterated_text
 
